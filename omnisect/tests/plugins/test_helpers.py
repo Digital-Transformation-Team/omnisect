4bf87a05da2f5b432b7c0afdb89f10772e999cc9
bl1nkker <nurovich14@gmail.com>
2025-09-03 16:16:57 +0500

    test: removed tests

--- omnisect/tests/plugins/test_helpers.py
+++ omnisect/tests/plugins/test_helpers.py
@@ -1,22 +1,22 @@
-from plugins.helpers import FileSystem
-from tests.fixtures.file_fixtures import FileFixtures
+# from plugins.helpers import FileSystem
+# from tests.fixtures.file_fixtures import FileFixtures
 
 
-class TestHelpers(FileFixtures):
-    def test_file_system_get_plugins_directory(self):
-        assert FileSystem.get_plugins_directory().endswith("plugins/plugins")
+# class TestHelpers(FileFixtures):
+#     def test_file_system_get_plugins_directory(self):
+#         assert FileSystem.get_plugins_directory().endswith("plugins/plugins")
 
-    def test_file_system_load_configuration(self, fs_config):
-        assert FileSystem.load_configuration(
-            name="configuration.yaml",
-            config_directory=fs_config.test_data_path,
-        ) == {
-            "logging": {"level": "DEBUG"},
-            "plugins": ["advance-plugin", "sample-plugin"],
-            "registry": {
-                "name": "",
-                "repository": "",
-                "tag": "latest",
-                "url": "https://github.com/{name}/{repository}/releases/download/{tag}",
-            },
-        }
+#     def test_file_system_load_configuration(self, fs_config):
+#         assert FileSystem.load_configuration(
+#             name="configuration.yaml",
+#             config_directory=fs_config.test_data_path,
+#         ) == {
+#             "logging": {"level": "DEBUG"},
+#             "plugins": ["advance-plugin", "sample-plugin"],
+#             "registry": {
+#                 "name": "",
+#                 "repository": "",
+#                 "tag": "latest",
+#                 "url": "https://github.com/{name}/{repository}/releases/download/{tag}",
+#             },
+#         }
--- omnisect/tests/plugins/test_plugin_use_case_service.py
+++ omnisect/tests/plugins/test_plugin_use_case_service.py
@@ -1,14 +1,14 @@
-from unittest.mock import patch
+# from unittest.mock import patch
 
-from plugins.plugin_use_case_service import PluginUseCase
+# from plugins.plugin_use_case_service import PluginUseCase
 
 
-class TestPluginUseCaseService:
-    @patch("plugins.utils.subprocess.check_call")
-    @patch("plugins.utils.distributions")
-    def test_discover_plugins(self, mock_distributions, mock_check_call):
-        mock_distributions.return_value = []
-        mock_check_call.return_value = 0
-        use_case = PluginUseCase()
-        use_case.discover_plugins(reload=True)
-        assert len(use_case.modules) >= 1
+# class TestPluginUseCaseService:
+#     @patch("plugins.utils.subprocess.check_call")
+#     @patch("plugins.utils.distributions")
+#     def test_discover_plugins(self, mock_distributions, mock_check_call):
+#         mock_distributions.return_value = []
+#         mock_check_call.return_value = 0
+#         use_case = PluginUseCase()
+#         use_case.discover_plugins(reload=True)
+#         assert len(use_case.modules) >= 1
