999472cf9e21dde5af67179b7c333628a4c7a5f8
bl1nkker <nurovich14@gmail.com>
2025-09-04 16:37:53 +0500

    feat: added error handler

--- omnisect/web/app.py
+++ omnisect/web/app.py
@@ -2,12 +2,22 @@ from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
 
 from dishka.integrations.fastapi import setup_dishka
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.staticfiles import StaticFiles
 
 from app_config import get_app_config
 from ioc import create_async_ioc_container
 from ioc.provider_registry import get_providers
+from src.errors.types import (
+    ForbiddenError,
+    InvalidContentTypeError,
+    InvalidCredentialsError,
+    InvalidDataError,
+    InvalidTokenError,
+    NotFoundError,
+    ValidationError,
+)
+from web.errors.handler import generate_default_error_handler
 from web.routers import plugins_router
 
 
@@ -18,8 +28,21 @@ async def lifespan(app: FastAPI) -> AsyncIterator[None]:
     # https://dishka.readthedocs.io/en/stable/integrations/fastapi.html
 
 
-def create_web_application():
+def create_web_application(
+    errors_mapping: dict[type[Exception], int] | None = None,
+):
     app = FastAPI(lifespan=lifespan)
+    em: dict[type[Exception], int] = {
+        ValidationError: status.HTTP_400_BAD_REQUEST,
+        InvalidDataError: status.HTTP_400_BAD_REQUEST,
+        InvalidTokenError: status.HTTP_401_UNAUTHORIZED,
+        InvalidCredentialsError: status.HTTP_401_UNAUTHORIZED,
+        InvalidContentTypeError: status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        NotFoundError: status.HTTP_404_NOT_FOUND,
+        ForbiddenError: status.HTTP_403_FORBIDDEN,
+    }
+    em.update(errors_mapping or dict())
+    app.add_exception_handler(Exception, generate_default_error_handler(em))
     app.mount("/outputs", StaticFiles(directory="plugins/outputs"), name="outputs")
     app.include_router(plugins_router.router)
     config = get_app_config()
